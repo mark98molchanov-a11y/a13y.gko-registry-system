9078c9dd9b9ca1c83d9ea7cf971f7527f6c8621f
mark98molchanov-a11y <mark98molchanov@gmail.com>
2026-07-20 17:56:19 +0500

    Update map-tab.js

--- js/map-tab.js
+++ js/map-tab.js
@@ -145,6 +145,15 @@ const locationIndex = headers.indexOf('location');
         // ✅ ОЧИЩАЕМ МАССИВЫ ПРИ ПОВТОРНОЙ ЗАГРУЗКЕ
         allDealsFlat = [];
         originalAllDealsFlat = [];
+         dealsData = {};
+        dealTypes = {};
+        cityTypes = {};
+        objectTypes = {};
+        wallMaterialTypes = {};
+        quarterTypes = {};
+        yearBuildTypes = {};
+        purposeCount = {};
+        vriCount = {};
         
         for (let i = 1; i < lines.length; i++) {
             const values = parseCSVLine(lines[i]);
