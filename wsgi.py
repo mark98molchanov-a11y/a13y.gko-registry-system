7e91bb4d2b9678043e7570a3692e6e511afe5c50
mark98molchanov-a11y <mark98molchanov@gmail.com>
2026-08-17 17:15:51 +0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,11 @@
+import sys
+print("🔥🔥🔥 WSGI.PY ЗАПУЩЕН!", flush=True)
+sys.stdout.flush()
+
 from app import app
+
+print("✅ WSGI: app импортирован!", flush=True)
+sys.stdout.flush()
 
 if __name__ == "__main__":
     app.run()
