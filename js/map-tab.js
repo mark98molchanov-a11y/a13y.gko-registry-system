5950af09e5711d08cc4713bee453d0a28affa281
mark98molchanov-a11y <mark98molchanov@gmail.com>
2026-09-23 09:07:12 +0500

    Update map-tab.js

--- js/map-tab.js
+++ js/map-tab.js
@@ -3988,7 +3988,7 @@ function initMapTab(containerId) {
 
     window.mapInstance.attributionControl.remove();
 
-   L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png?key=cb1_3uly_1_5f67cc4b44300afbb4afba80', {
     attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>, © <a href="https://carto.com/attributions">CARTO</a>',
     subdomains: 'abcd',
     maxZoom: 20
